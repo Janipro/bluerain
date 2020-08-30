3262ef434d55b3d1d2516f1d111c86c95b853c34
Jan Adrian Hafskjold <jan.hafskjold@gmail.com>
2020-08-30 19:54:48 +0200

    Added support for new items

---
+++ src/main/java/com/jani/guttamod/registry/KpItem.java
@@ -0,0 +1,21 @@
+package com.jani.guttamod.registry;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
+
+public class KpItem extends Item {
+    public KpItem(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity playerEntity, Hand hand) {
+        playerEntity.playSound(ModItems.KP_EVENT, 1.0F, 1.0F);
+        return new TypedActionResult<>(ActionResult.SUCCESS, playerEntity.getStackInHand(hand));
+    }
+}
